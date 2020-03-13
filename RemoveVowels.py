eaf056de0c4af7341e351b34ecd2708f244076c9
FreakoutGames <contactfreakout@gmail.com>
2020-03-13 16:47:08 +0530

    Program to remove vowels from a string

---
+++ RemoveVowels.py
@@ -0,0 +1,16 @@
+#author: Vijesh.V  @FreakoutGames
+#program to remove vowels from a string
+
+def removeVowels(string):
+    vowels = "aeiou"
+    newstring = list(string)
+    for i in vowels:
+        length = len(newstring)
+        for j in range(0, length):
+            if newstring[j] == i.upper() or newstring[j] == i.lower():
+                newstring[j]=""
+                length = len(newstring)
+    string = ''.join(newstring)
+    return string
+
+print(removeVowels("This program is for losers LOL!"))
